d654fdc420d4c1d69ded5780f6fd8cbbe1fffe29
Ahmet Unsal <iletisim.unsalahmet@gmail.com>
2024-09-22 01:44:28 +0300

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import MultiSelect from "@/components/multi-select";
+import Image from "next/image";
+import React, { useState } from "react";
+
+export default function Home() {
+  const [selectedValues, setSelectedValues] = useState<string[]>([]);
+
+  const handleValueChange = (value: string[]) => {
+    setSelectedValues(value);
+  };
+
+  const options = [
+    { value: "apple", label: "Apple" },
+    { value: "banana", label: "Banana" },
+    { value: "cherry", label: "Cherry" },
+    { value: "date", label: "Date" },
+    { value: "fig", label: "Fig" },
+  ];
+
+  return (
+    <div className="w-full h-screen bg-slate-800 flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-4xl">Shadcn Multiselect Example</h1>
+        <MultiSelect
+          options={options}
+          selectedOptions={selectedValues}
+          onChange={(selected: string[]) => {
+            handleValueChange(selected);
+          }}
+          placeholder="Please select.."
+          className="w-[95vw] md:w-96 mt-10"
+          contentClass="w-[95vw] md:!w-96"
+        />
+        <p className="w-1/2 mt-10">
+          I'm a junior, I'm just experiencing Frontend. I'm sharing it to give
+          you an idea. It's definitely not the final product. Have a good day.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ components/multi-select.tsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import { DropdownMenuCheckboxItemProps } from "@radix-ui/react-dropdown-menu";
+import { Input } from "./ui/input";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+type Checked = DropdownMenuCheckboxItemProps["checked"];
+
+const MultiSelect = (props: {
+  options: { value: string; label: string, disabled?: boolean }[];
+  selectedOptions: string[];
+  placeholder: string;
+  onChange: (selectedOptions: string[]) => void; // Callback fonksiyonu
+  className?: string; 
+  contentClass?: string;
+  required?: boolean | false;
+}) => {
+  const [selectedOptions, setSelectedOptions] = useState<string[]>(props.selectedOptions);
+
+  useEffect(() => {
+    setSelectedOptions(props.selectedOptions);
+  }, [props.selectedOptions]);
+
+  const handleCheckedChange = (optionValue: string, checked: Checked) => {
+    const updatedSelectedOptions = checked
+      ? [...selectedOptions, optionValue]
+      : selectedOptions.filter((value) => value !== optionValue);
+
+    setSelectedOptions(updatedSelectedOptions);
+    props.onChange(updatedSelectedOptions); // Callback çağrısı
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger
+        asChild
+        className={`flex justify-between items-center hover:cursor-pointer ${props.className}`}
+      >
+        <div className="flex justify-between items-center text-white">
+          <Input
+            required={props.required}
+            className="w-full text-white text-start hover:cursor-pointer"
+            placeholder={props.placeholder}
+            value={selectedOptions
+              .map((value) => props.options.find((option) => option.value === value)?.label || "")
+              .join(", ")}
+            readOnly
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className={`w-[95vw] xl:w-48 lg:w-96 dark ${props.contentClass}`}>
+        {props.options.map((option) => (
+          <DropdownMenuCheckboxItem
+            className="w-full"
+            key={option.value}
+            disabled={option.disabled}
+            checked={selectedOptions.includes(option.value)}
+            onCheckedChange={(checked: any) => handleCheckedChange(option.value, checked)}
+          >
+            {option.label}
+          </DropdownMenuCheckboxItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default MultiSelect;
